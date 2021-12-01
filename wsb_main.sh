6460b99ce7ea3154ddc1b2db6f6b1b56ba5172ea
ohammond1 <ohammond1@gmail.com>
2021-12-01 16:22:09 -0600

    Adding sub, main, and R testing. The tidyverse and sentiment R package is too big to git

---
+++ wsb_main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# untar your R installation
+tar -xzf R402.tar.gz
+tar -xzf packages_tidyverse_sentimentr.tar.gz
+
+# make sure the script will use your R installation, 
+# and the working directory as its home location
+export PATH=$PWD/R/bin:$PATH
+export RHOME=$PWD/R
+export R_LIBS=$PWD/packages
+
+#fname=${2%.*}
+
+#tar -xf "${fname}.tgz"
+
+Rscript wsb_test.R
---
+++ wsb_test.R
@@ -0,0 +1,2 @@
+require(sentimentr)
+require(tidyverse)
