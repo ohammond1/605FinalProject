0baa0dd522193f8f6ef1d9ab92d753b884a8d6a6
maritmcquaig <marit.mcquaig@gmail.com>
2021-12-05 10:18:32 -0600

    Splitting second dataset dec 2020 - feb 2021

--- wsb_split.py
+++ wsb_split.py
@@ -13,3 +13,16 @@ with pd.read_csv(filename, chunksize=chunksize, low_memory=False,usecols=['body'
         if i % 2 == 0:
             print(i)
             print(datetime.datetime.now())
+
+
+#to read in second dataset, starting with 35 to pick up where last chunks left off
+file = './wallstreetbets_comments.csv'
+j = 35
+with pd.read_csv(file, chunksize=chunksize,usecols=['body','created_utc'],low_memory=False) as reader:
+    for chunk in reader:
+        outfile = 'wsb_chunk' + str(j) + '.csv'
+        chunk.to_csv(outfile,index=False)
+        j += 1
+        if j % 2 == 0:
+            print(j)
+            print(datetime.datetime.now())
