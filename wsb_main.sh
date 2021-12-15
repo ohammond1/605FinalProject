15029a55fae43dee7dd526a2f1cb85cd08aa3a9d
ohammond1 <ohammond1@gmail.com>
2021-12-15 09:08:24 -0600

    Adjusting CHTC code to work with smaller R packages

--- wsb_main.sh
+++ wsb_main.sh
@@ -2,7 +2,7 @@
 
 # untar your R installation
 tar -xzf R402.tar.gz
-tar -xzf packages_tidyverse_sentimentr.tar.gz
+tar -xzf packages_dplyr_lub_sentr.tar.gz
 
 # make sure the script will use your R installation, 
 # and the working directory as its home location
@@ -13,5 +13,13 @@ export R_LIBS=$PWD/packages
 #fname=${2%.*}
 
 #tar -xf "${fname}.tgz"
+wget https://github.com/ohammond1/605FinalProject/raw/main/data_tar/${1}
 
-Rscript SentimentCode.R $1
+tar -xf $1
+fname=${1%.tar.gz}
+
+
+Rscript SentimentCode.R "${fname}.csv"
+
+rm -rf $1
+rm -rf "${fname}.csv"
